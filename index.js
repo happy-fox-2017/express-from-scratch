0450428014a7308c1d4d3825230282aaf560f66d
myyusuf <myyusuf1911@gmail.com>
2017-06-11 05:32:42 +0700

    Release: 3

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send('I Love Hactiv8!');
-});
+app.use('/', routes);
 
 app.listen(3000);
