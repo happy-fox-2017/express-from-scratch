f70997fc0e71cb1430dbfe9c272291337c46436d
myyusuf <myyusuf1911@gmail.com>
2017-06-11 05:51:24 +0700

    Release: 4

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,24 @@ router.get('/', function(req, res) {
   res.send('I Love Hactiv8!');
 });
 
+router.get('/user', function(req, res) {
+  res.send('User page');
+});
+
+router.post('/user', function(req, res) {
+  res.send('User post form');
+});
+
+router.get('/user/edit/:id', function(req, res) {
+  res.send('User edit page');
+});
+
+router.post('/user/edit/:id', function(req, res) {
+  res.send('User edit form');
+});
+
+router.get('/user/delete/:id', function(req, res) {
+  res.send('User delete');
+});
+
 module.exports = router;
